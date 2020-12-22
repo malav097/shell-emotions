afa553d4b215f3189897fbd5f1926f744ec11e6f
avanishsubbiah <avanishsubbiah1@gmail.com>
2020-12-22 15:28:09 -0500

    Cleaned up Threading

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ awake_frames = []
 sleepy_ind = [0, 7]
 awake_ind = [0, 1, 4, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 5, 6, 7]
 cpu_percent = 0
-flag_busy = 0
+state = 0
 frames_path = "./assets/frames/"
 working_dir = os.path.dirname(__file__)
 
@@ -34,32 +34,25 @@ for i in range(5):
 # Method to refresh utilization values
 def check_usage(thread_name):
     global cpu_percent
+    global state
     while True:
-        flag_busy = 1
         cpu_percent = psutil.cpu_percent(1)
-        flag_busy = 0
+        if (cpu_percent <= 10): # Sleepy Animation
+            state = 0
+        elif (cpu_percent > 10 and cpu_percent < 30): # Waking Up
+            state = 1
+        elif (cpu_percent >= 30): # Fully Awake
+            state = 2
         time.sleep(5)
 
 # Method to print and update emotion animation
 def emote(thread_name):
-    prev_state = 0
     while True:
-        if (flag_busy == 1):
-            state = prev_state
-        else:
-            if (cpu_percent <= 10): # Sleepy Animation
-                state = 0
-            elif (cpu_percent > 10 and cpu_percent < 20): # Waking Up
-                state = 1
-            elif (cpu_percent >= 30):
-                state = 2
-        
         if (state == 0): # Sleepy Animation
             for frame in sleepy_frames:
                 os.system('printf "\033c"')
                 print("".join(frame))
                 time.sleep(0.2)
-            prev_state = 0
         elif (state == 1): # Waking Up
             for frame in frames:
                 #clear the shell
@@ -68,13 +61,11 @@ def emote(thread_name):
                 #print(" TEST ")
                 time.sleep(0.2)
                 #clear the shell
-            prev_state = 1
         elif (state == 2):
             for frame in awake_frames:
                 os.system('printf "\033c"')
                 print("".join(frame))
                 time.sleep(0.2)
-            prev_state = 2
 
 # Starting threads
 util_thread = Thread(target=check_usage, args=("UTILIZATION CHECK THREAD",))
@@ -82,6 +73,3 @@ emote_thread = Thread(target=emote, args=("EMOTION PRINT THREAD",))
 
 util_thread.start()
 emote_thread.start()
-util_thread.join()
-emote_thread.join()
-
