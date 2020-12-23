ae83651bc6bb2aace77a15b58e1ab3dda05eb979
avanishsubbiah <avanishsubbiah1@gmail.com>
2020-12-22 23:50:40 -0500

    Added lib folder and Animation class

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-import os , time
-import psutil
+import os , time, psutil, signal, sys
 from threading import Thread
-import signal
-import sys
+from lib.animation import Animation
 
 
 # Importing frames and Initializing file paths and global state
-files   = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
-frames  = []
-sleepy_frames = []
-awake_frames = []
-rage_frames = []
-sleepy_ind = [0, 7]
-awake_ind = [0, 1, 4, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 3, 9, 5, 6, 7]
+emotion_names = ["sleep", "waking", "awake", "rage"]
+emotions = {}
 state = 0
 frames_path = "./assets/frames/"
-working_dir = os.path.dirname(__file__)
-
-# Opening files and setting up animation frame arrays
-for name in files:
-    filepath = frames_path + name
-    print(filepath)
-    rel_path = os.path.relpath(filepath, working_dir)
-    with open(rel_path, "r", encoding="utf8") as f:
-        #sabe every line in list "f"
-        f = f.readlines()
-        #append list f to list frames
-        frames.append(f)
-
-for i in range(18):
-    filepath = frames_path + "rage/" + str(i)
-    print(filepath)
-    rel_path = os.path.relpath(filepath, working_dir)
-    with open(rel_path, "r", encoding="utf8") as f:
-        #sabe every line in list "f"
-        f = f.readlines()
-        #append list f to list frames
-        rage_frames.append(f)
-
-for ind in awake_ind: # Setting up Awake frames
-    awake_frames.append(frames[ind])
-
-for i in range(5): # Setting up Asleep frames
-    for ind in sleepy_ind:
-        sleepy_frames.append(frames[ind])
 
 
 # Method to refresh utilization values
@@ -51,43 +15,32 @@ def state_update(thread_name):
     global state
     while True:
         cpu_percent = psutil.cpu_percent(1)
-        if (cpu_percent <= 10): # Sleepy Animation
-            state = 0
-        elif (cpu_percent > 10 and cpu_percent <= 30): # Waking Up
-            state = 1
-        elif (cpu_percent > 30 and cpu_percent <= 90): # Fully Awake
-            state = 2
+        if (cpu_percent <= 10): # Sleep
+            state = emotions["sleep"].id
+        elif (cpu_percent > 10 and cpu_percent <= 30): # Waking
+            state = emotions["waking"].id
+        elif (cpu_percent > 30 and cpu_percent <= 90): # Awake
+            state = emotions["awake"].id
         elif (cpu_percent > 90): # Rage
-            state = 3
+            state = emotions["rage"].id
         time.sleep(5)
+
+
+# Method to print frames given a key
+def print_frames(key):
+    frames = emotions[key].frames
+    for frame in frames:
+        os.system('printf "\033c"')
+        print("".join(frame))
+        time.sleep(0.2)
 
 
 # Method to print and update emotion animation
 def emote(thread_name):
     while True:
-        if (state == 0): # Sleepy Animation
-            for frame in sleepy_frames:
-                os.system('printf "\033c"')
-                print("".join(frame))
-                time.sleep(0.2)
-        elif (state == 1): # Waking Up
-            for frame in frames:
-                #clear the shell
-                os.system('printf "\033c"') #printf "\033c"') 
-                print("".join(frame))
-                #print(" TEST ")
-                time.sleep(0.2)
-                #clear the shell
-        elif (state == 2): # Awake
-            for frame in awake_frames:
-                os.system('printf "\033c"')
-                print("".join(frame))
-                time.sleep(0.2)
-        elif (state == 3): # Rage
-            for frame in rage_frames:
-                os.system('printf "\033c"')
-                print("".join(frame))
-                time.sleep(0.2)
+        for key, val in emotions.items():
+            if (state == val.id):
+                print_frames(key)
 
 
 # Method to shutdown program
@@ -101,7 +54,9 @@ def main():
     signal.signal(signal.SIGTERM, shutdown)
     signal.signal(signal.SIGINT, shutdown)
 
-    # Starting threads
+    for i in range(len(emotion_names)):
+        emotions.update({emotion_names[i] : Animation(emotion_names[i], i, frames_path)})
+
     util_thread = Thread(target=state_update, args=("STATE UPDATE THREAD",))
     emote_thread = Thread(target=emote, args=("EMOTION PRINT THREAD",))
     util_thread.setDaemon(True)
@@ -111,6 +66,7 @@ def main():
     emote_thread.start()
     util_thread.join()
     emote_thread.join()
+
 
 if __name__ == "__main__":
     main()
